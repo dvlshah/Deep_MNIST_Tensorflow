b17180381ae6be037ed4222be6f9b660187d4153
Deval Shah <dvlshah@users.noreply.github.com>
2018-04-11 14:18:54 +0530

    Delete update.sh

--- update.sh
+++
@@ -1,6 +0,0 @@
-echo "# Deep_MNIST_Tensorflow" >> README.md
-git init
-git add .
-git commit -m "commit"
-git remote add origin git@github.com:dvlshah/Deep_MNIST_Tensorflow.git
-git push -f origin master
