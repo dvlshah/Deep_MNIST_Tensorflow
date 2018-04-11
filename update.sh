e38cbf5186a2d55747b510dcf35177bf75b83b24
Deval Shah <devalshah1619@gmail.com>
2018-04-11 14:11:44 +0530

    commit

---
+++ update.sh
@@ -0,0 +1,6 @@
+echo "# Deep_MNIST_Tensorflow" >> README.md
+git init
+git add .
+git commit -m "commit"
+git remote add origin git@github.com:dvlshah/Deep_MNIST_Tensorflow.git
+git push -f origin master
